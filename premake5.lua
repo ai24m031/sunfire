cda6d852c24adb2e99241588051220e7c9250a72
ai24m031 <ai24m031@technikum-wien.at>
2024-11-11 22:50:35 +0100

    Premake

---
+++ premake5.lua
@@ -0,0 +1,115 @@
+workspace "Sunfire"
+	architecture "x64"
+
+	configurations 
+	{ 
+		 "Debug", 
+		 "Release",
+		 "Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Sunfire"
+	location "Sunfire"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
+
+		defines 
+		{
+			"SUNFIRE_PLATFORM_WINDOWS",
+			"SUNFIRE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	buildoptions "/utf-8"
+
+	filter "configurations:Debug"
+		defines "SUNFIRE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SUNFIRE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SUNFIRE_DIST"
+		optimize "On"
+
+	filter { "system:windows", "configurations:Release"}
+		buildoptions "/MT"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Sunfire/vendor/spdlog/include",
+		"Sunfire/src"
+	}
+
+	links
+	{
+		"Sunfire"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
+
+		defines 
+		{
+			"SUNFIRE_PLATFORM_WINDOWS"
+		}
+
+	buildoptions "/utf-8"
+
+	filter "configurations:Debug"
+		defines "SUNFIRE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SUNFIRE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SUNFIRE_DIST"
+		optimize "On"
+
+	filter { "system:windows", "configurations:Release"}
+		buildoptions "/MT"
